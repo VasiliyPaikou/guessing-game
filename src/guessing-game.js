a38f8a3d190a90bc9fe5c019e0d13098dd6e7008
VasiliyPaikou <gtr208111@gmail.com>
2022-08-11 22:19:49 +0300

    feat:comlette task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+  constructor() {
+    this.min = 0
+    this.max = 0
+    this.nums = []
+  }
+  setRange(min, max) {
+    this.min = min
+    this.max = max
+    while (min !== max) {
+      this.nums.push(min)
+      min++
     }
+    this.nums.push(max)
+  }
+  guess() {
+    this.result = Math.round((this.max - this.min)/2) + this.min;
+      return this.result
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  }
+  lower() {
+    this.max = this.result
+  }
+  greater() {
+    this.min = this.result
 }
-
+}
 module.exports = GuessingGame;
